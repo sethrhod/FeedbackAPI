fb4578207d523cab92dcb4bace03cb2a7156eb44
Spadeful <iamsethrhodes@gmail.com>
2023-04-03 15:12:59 -0400

    removed ssl modules

--- app.py
+++ app.py
@@ -3,12 +3,6 @@ from submit_feedback import submit_feedback
 from get_feedback import get_feedback
 from update_feedback import update_feedback
 from create_DB import create_DB
-import ssl
-
-priv_key = '/home/seth/certs/myCA.key'
-cert = '/home/seth/certs/myCA.pem'
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain(cert, priv_key)
 
 app = Flask(__name__)
 
@@ -35,4 +29,4 @@ def feedback():
 
 if __name__ == '__main__':
     create_DB()
-    app.run(debug=True, ssl_context=context)
+    app.run(debug=True)
