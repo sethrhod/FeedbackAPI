2fa4b02cf05b9e0df7206142f7917fdc086c4da5
Spadeful <iamsethrhodes@gmail.com>
2023-04-04 12:11:58 -0400

    added config.json

--- create_DB.py
+++ create_DB.py
@@ -1,8 +1,18 @@
 import psycopg2
+import json
 
-def create_DB():
-    conn = psycopg2.connect("dbname=test user=postgres")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE if NOT EXISTS feedback (userid uuid, sessionid uuid, feedback VARCHAR(255))")
-    conn.commit()
-    conn.close()
+## a function that connects to a postgres database hosted in azure and creates a table if not exists
+
+def create_table():
+
+    with open('config.json') as f:
+        config = json.load(f)
+
+    try:
+        conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(config['<database-name>'], config['<admin-username>'], config['<server-name>'], config['<admin-password>']))
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS feedback (userid varchar(255), sessionid varchar(255), feedback varchar(255));")
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(e)
