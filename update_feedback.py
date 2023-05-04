e40a641b5d963c850af2e8434a29dd174d4b0d2f
Spadeful <iamsethrhodes@gmail.com>
2023-05-04 13:22:46 -0400

    Added delete feedback method

--- update_feedback.py
+++ update_feedback.py
@@ -1,13 +1,16 @@
 import psycopg2
 import json
 
-def update_feedback(userid, sessionid, feedback):
+
+def update_feedback(userid, sessionid, feedback, original_feedback):
     with open('config.json') as f:
         config = json.load(f)
 
-    conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(config['<database-name>'], config['<admin-username>'], config['<server-name>'], config['<admin-password>']))
+    conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(
+        config['<database-name>'], config['<admin-username>'], config['<server-name>'], config['<admin-password>']))
     cur = conn.cursor()
-    cur.execute("UPDATE feedback SET text = %s WHERE userid = %s AND sessionid = %s", (feedback, userid, sessionid))
+    cur.execute("UPDATE feedback SET text = %s WHERE userid = %s AND sessionid = %s AND text = %s",
+                (feedback, userid, sessionid, original_feedback))
     conn.commit()
     conn.close()
     return "Feedback updated successfully"
